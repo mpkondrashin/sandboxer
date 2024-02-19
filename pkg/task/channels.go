25236ed010009329d17de3a99795c3732d6d7292
Michael Kondrashin <mkondrashin@gmail.com>
2024-02-19 09:09:15 +0200

    change statuses (add channels)

---
+++ pkg/task/channels.go
@@ -0,0 +1,113 @@
+/*
+Sandboxer (c) 2024 by Mikhail Kondrashin (mkondrashin@gmail.com)
+Software is distributed under MIT license as stated in LICENSE file
+
+channels.go
+
+Channels to dispatchers to communicate
+*/
+package task
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	ChannelSize = 1000
+)
+
+type Channel int
+
+const (
+	ChPrefilter Channel = iota
+	ChSubmit
+	ChWait
+	ChResult
+	ChReport
+	ChInvestigation
+	ChDone
+)
+
+var ChannelString = [...]string{
+	"Prefilter",
+	"Submission",
+	"Wait",
+	"Get Result",
+	"Get Report",
+	"Get Investigation",
+	"Done",
+}
+
+// String - return string representation for State value
+func (c Channel) String() string {
+	if c < 0 || c > ChDone {
+		return "Channel(" + strconv.FormatInt(int64(c), 10) + ")"
+	}
+	return ChannelString[c]
+}
+
+// ErrUnknownState - will be returned wrapped when parsing string
+// containing unrecognized value.
+var ErrUnknownChannel = errors.New("unknown Channel")
+
+var mapChannelFromString = map[string]Channel{
+	ChannelString[ChPrefilter]:     ChPrefilter,
+	ChannelString[ChSubmit]:        ChSubmit,
+	ChannelString[ChWait]:          ChWait,
+	ChannelString[ChResult]:        ChResult,
+	ChannelString[ChReport]:        ChReport,
+	ChannelString[ChInvestigation]: ChInvestigation,
+	ChannelString[ChDone]:          ChDone,
+	ChannelString[ChPrefilter]:     ChPrefilter,
+}
+
+// UnmarshalJSON implements the Unmarshaler interface of the json package for State.
+func (c *Channel) UnmarshalJSON(data []byte) error {
+	var v string
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	result, ok := mapChannelFromString[v]
+	if !ok {
+		return fmt.Errorf("%w: %s", ErrUnknownChannel, v)
+	}
+	*c = result
+	return nil
+}
+
+// MarshalJSON implements the Marshaler interface of the json package for State.
+func (c Channel) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%v\"", c)), nil
+}
+
+type IDChannel chan ID
+
+type Channels struct {
+	TaskChannel [ChDone]chan ID
+}
+
+func NewChannels() *Channels {
+	c := &Channels{}
+	for i := ChPrefilter; i < ChDone; i++ {
+		c.TaskChannel[i] = make(chan ID, ChannelSize)
+	}
+	return c
+}
+
+func (c *Channels) Close() {
+	for i := ChPrefilter; i < ChDone; i++ {
+		close(c.TaskChannel[i])
+	}
+}
+
+func (c *Channels) String() string {
+	var sb strings.Builder
+	for i := ChPrefilter; i < ChDone; i++ {
+		fmt.Fprintf(&sb, "%d - ", len(c.TaskChannel[i]))
+	}
+	return sb.String()
+}
---
+++ pkg/task/task_test.go
@@ -0,0 +1,43 @@
+package task
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestHashes(t *testing.T) {
+	text := "Hello World!"
+	folder := filepath.Join("tests", "hashes")
+	if err := os.MkdirAll(folder, 0755); err != nil {
+		t.Fatal(err)
+	}
+	fileName := "file.txt"
+	filePath := filepath.Join(folder, fileName)
+	if err := os.WriteFile(filePath, []byte(text), 0644); err != nil {
+		t.Fatal(err)
+	}
+	tsk := NewTask(0, filePath)
+	if err := tsk.CalculateHash(); err != nil {
+		t.Fatal(err)
+	}
+
+	var actual, expected string
+	expected = "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069"
+	actual = tsk.SHA256
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+
+	expected = "2ef7bde608ce5404e97d5f042f95f89f1c232871"
+	actual = tsk.SHA1
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+
+	expected = "ed076287532e86365e841e92bfc50d8c"
+	actual = tsk.MD5
+	if expected != actual {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
