ba13a10b833c4ee9601668e01da92bd1b15ad912
Michael Kondrashin <mkondrashin@gmail.com>
2024-02-29 13:07:23 +0200

    show error windows on start

---
+++ pkg/fatal/fatal.go
@@ -0,0 +1,21 @@
+package fatal
+
+import (
+	"sandboxer/pkg/globals"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Warning(title, text string) {
+	//dialog.ShowInformation("Information", "This is a sample message", s.window)
+	a := app.New()
+	w := a.NewWindow(globals.AppName + " " + title)
+
+	w.SetContent(container.NewVBox(
+		widget.NewLabel(text),
+		widget.NewButton("Ok", func() { a.Quit() }),
+	))
+	w.ShowAndRun()
+}
