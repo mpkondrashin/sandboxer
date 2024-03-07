e8353b424c2f5c80d4f110d7a8b8fe1b501d7524
Michael Kondrashin <mkondrashin@gmail.com>
2024-03-07 07:39:24 +0200

    unify vone settings widgets

---
+++ pkg/settings/vone_settings.go
@@ -0,0 +1,92 @@
+package settings
+
+import (
+	"context"
+	"sandboxer/pkg/config"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+	"github.com/mpkondrashin/vone"
+)
+
+const ErrorDomain = "Select"
+
+type VisionOne struct {
+	conf *config.VisionOne
+
+	tokenEntry *widget.Entry
+	//domainLabel      *widget.Label
+	//chooseLabel      *widget.Label
+	visionOneDomains *widget.Select
+	cancelDetect     context.CancelFunc
+}
+
+func NewVisionOne(conf *config.VisionOne) *VisionOne {
+	return &VisionOne{
+		conf: conf,
+	}
+}
+
+func (s *VisionOne) Widget() fyne.CanvasObject {
+
+	s.tokenEntry = widget.NewMultiLineEntry()
+	s.tokenEntry.SetText(s.conf.Token)
+	s.tokenEntry.Wrapping = fyne.TextWrapBreak
+	s.tokenEntry.OnChanged = s.DetectDomain
+	tokenFormItem := widget.NewFormItem("Token:", s.tokenEntry)
+	tokenFormItem.HintText = "Go to Administrator -> API Keys"
+	//roleHint := "Go to Administration -> User Roles -> Permissions -> Threat Intelligence -> Sandbox Analysis -> \"View, filter, and search\" and\"Submit object\""
+	// apiKeyHitn := "Go to Administration -> API Keys -> Add API Key"
+
+	//s.domainLabel = widget.NewLabel(s.conf.Domain)
+	//domainFormItem := widget.NewFormItem("Domain:", s.domainLabel)
+	//s.chooseLabel = widget.NewLabel("Choose Vision One Domain:")
+	domains := []string{ErrorDomain}
+	for _, d := range vone.RegionalDomains {
+		//l := fmt.Sprintf("%s (%s)", d.Region, d.Domain)
+		domains = append(domains, d.Domain)
+	}
+
+	s.visionOneDomains = widget.NewSelect(domains, nil)
+	if s.conf.Domain != "" {
+		s.visionOneDomains.SetSelected(s.conf.Domain)
+	}
+	domainFormItem := widget.NewFormItem("Domain:", s.visionOneDomains)
+	optionsForm := widget.NewForm(
+		tokenFormItem,
+		domainFormItem,
+	)
+	return optionsForm
+}
+
+func (s *VisionOne) Update() {
+	s.DetectDomain(s.tokenEntry.Text)
+}
+
+func (s *VisionOne) DetectDomain(token string) {
+	go func() {
+		if s.cancelDetect != nil {
+			s.cancelDetect()
+		}
+		var ctx context.Context
+		ctx, s.cancelDetect = context.WithCancel(context.TODO())
+		defer func() {
+			if s.cancelDetect != nil {
+				s.cancelDetect()
+			}
+			s.cancelDetect = nil
+		}()
+		domain := vone.DetectVisionOneDomain(ctx, token)
+		if domain != "" {
+			s.visionOneDomains.SetSelected(domain)
+		}
+	}()
+}
+
+func (s *VisionOne) Aquire() {
+	s.conf.Token = strings.TrimSpace(s.tokenEntry.Text)
+	if s.visionOneDomains.Selected != "" {
+		s.conf.Domain = s.visionOneDomains.Selected
+	}
+}
